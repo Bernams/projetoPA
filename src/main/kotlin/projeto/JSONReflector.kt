6e61b9388df8bc0d36ee2011117c8c16c5eefbce
miguelaguiar <miguel.aguiar.01@gmail.com>
2023-04-26 14:48:15 +0100

    JSON Reflector implemented

--- src/main/kotlin/projeto/JSONReflector.kt
+++ src/main/kotlin/projeto/JSONReflector.kt
@@ -1,40 +1,41 @@
 package projeto
 
+import JSONCustomKey
 import JSONExclude
 import projeto.jsonObjects.*
+import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.memberProperties
 
 class JSONReflector {
 
     fun reflect(obj : Any?) : JSONElement {
-        when (obj) {
-            null -> return JSONString(null)
-            is String, is Char -> return JSONString(obj.toString())
-            is Number -> return JSONNumber(obj)
-            is Boolean -> return JSONBoolean(obj)
-            is Enum<*> -> return JSONString(obj.name)
+        if(obj == null) return JSONString(null)
+        return when (obj) {
+            is String, is Char -> JSONString(obj.toString())
+            is Number -> JSONNumber(obj)
+            is Boolean -> JSONBoolean(obj)
+            is Enum<*> -> JSONString(obj.name)
             is Iterable<*> -> {
                 val jsonArray = JSONArray()
                 obj.forEach{jsonArray.add(reflect(it))}
-                return jsonArray
+                jsonArray
             }
             is Map<*, *> -> {
                 val jsonObject = JSONObject()
                 obj.forEach { (k, v) -> jsonObject.put(k.toString(), reflect(v)) }
-                return jsonObject
+                jsonObject
             }
             else -> {
                 val jsonObject = JSONObject()
                 for (prop in obj::class.memberProperties) {
-                    if (prop.hasAnnotation<JSONExclude>()) {
-                        continue
-                    }
-                    val name = prop.name
-                    val value = prop.getter.call(obj)
-                    jsonObject.put(name, reflect(value))
+                    val name =
+                        if (prop.hasAnnotation<JSONCustomKey>()) prop.findAnnotation<JSONCustomKey>()!!.identifier
+                        else prop.name
+                    if (prop.hasAnnotation<JSONExclude>()) continue
+                    jsonObject.put(name, reflect(prop.getter.call(obj)))
                 }
-                return jsonObject
+                jsonObject
             }
         }
     }
