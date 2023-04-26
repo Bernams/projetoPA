9cc398df7e1817aed2594dc2cf502067c230867d
miguelaguiar <miguel.aguiar.01@gmail.com>
2023-04-26 14:48:15 +0100

    JSON Reflector implemented

--- src/test/kotlin/projeto/JSONReflectorTest.kt
+++ src/test/kotlin/projeto/JSONReflectorTest.kt
@@ -1,6 +1,7 @@
 package projeto
 
 import JSONExclude
+import JSONForceString
 import projeto.jsonObjects.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -69,11 +70,11 @@ class JSONReflectorTest {
 
     @Test
     fun objectTest() {
-        data class TestData(@JSONExclude val a: String, val b: Int, val c: Boolean)
+        data class TestData(@JSONExclude val a: String, @JSONForceString val b: Int, val c: Boolean)
         val obj = TestData("pa", 92448, true)
         val result = reflector.reflect(obj)
         assertEquals(JSONObject().apply {
-            put("b", JSONNumber(92448))
+            put("b", JSONString("92448"))
             put("c", JSONBoolean(true))
         }.toJSONString(), result.toJSONString())
     }
